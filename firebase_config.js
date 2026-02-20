113d8e27eee2b81e6f028ca96f7d62fce897eccb
Rafał Kukla <loliwtf.omg@gmail.com>
2026-02-20 21:55:44 +0100

    Add files via upload

---
+++ firebase_config.js
@@ -0,0 +1,12 @@
+// Wklej tutaj konfigurację z Firebase (Project settings -> Your apps -> Web app)
+// Uwaga: databaseURL jest wymagane (Realtime Database).
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyArGjDo3PAItB9fojKCrNiRW_HMf3BoXPA",
+  authDomain: "nadgodziny-ea1ce.firebaseapp.com",
+  databaseURL: "https://nadgodziny-ea1ce-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "nadgodziny-ea1ce",
+  storageBucket: "nadgodziny-ea1ce.firebasestorage.app",
+  messagingSenderId: "319391686767",
+  appId: "1:319391686767:web:1f9cd1ba96ae520597ed9a"
+  // measurementId: "G-TPK2ZHF12K" // opcjonalnie
+};
